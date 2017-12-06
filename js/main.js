b1f8f919663ee9e5d00b3b786235498d6e4e11b6
alex-uy <alexuydesign@gmail.com>
2017-12-06 15:25:41 +1100

    Initial Portfolio Build

--- js/main.js
+++ js/main.js
@@ -1,10 +1,4 @@
-// $('#xIcon').hover(function () {
-//     $('#section1').stop(true, false).slideDown();
-// },
-
-// function () {
-//     $('#section1').stop(true, false).slideUp();
-// });
+	
 
 	function overlayShow() {
   		document.getElementById("overlay").className += " overlayContent";
@@ -14,30 +8,11 @@
 	  document.getElementById("overlay").classList.remove("overlayContent");
 	}
 
-
 $(document).ready(function() {
-
-
-
-	// // sections sliding up
-	// $('#xIcon').on('click', function(){
- //        $("#section1").slideUp(1000, function(){
-
- //        	//write event here for 'x' icon to now change class and be position absolute in the centre at top fixed (maybe fade in before rest of container does too to make natural transition)
- //        	$('#xIcon').addClass('stickyX')
- //        	console.log('test');
-
-
- //        	//main content fading in 
- //        	// $('#mainContent').fadeIn(1000);
- //        });    
- //    });
-
 	
 	// console.log($('#xIcon').offset().top);
 	var yOffset = $('#section-2').offset().top;
 
-		
 
 	// text animate
 	$(window).on('scroll', function () {  
@@ -56,16 +31,7 @@ $(document).ready(function() {
 		}
 
 
-	
-
-
-	// Subheading slide in animations
-	  
-	  // if( $('.animateText').offset().top < ( $(window).scrollTop() + ($(window).height() / 1.25) ) ){
-	  //   $('.animateText').addClass('visible');
-	  // }else{
-	  //   $('.animateText').removeClass('visible');
-	  // }
+	// Main Animations
 
 	  if( $('.animateTopFill').offset().top < ( $(window).scrollTop() + ($(window).height() / 1.3) ) ){
 	    $('.animateTopFill').addClass('visible');
@@ -105,7 +71,7 @@ $(document).ready(function() {
 	    $('.animateText3').removeClass('visible');
 	  }
 
-	  if( $('.animateText4').offset().top < ( $(window).scrollTop() + ($(window).height() / 4) ) ){  
+	  if( $('.animateText4').offset().top < ( $(window).scrollTop() + ($(window).height() / 2) ) ){  
 	    $('.animateText4').addClass('visible');
 	  }else{
 	    $('.animateText4').removeClass('visible');
@@ -143,10 +109,40 @@ $(document).ready(function() {
 
 
 
-
-
 	});
 });
+
+	// Portfolio Grid
+
+
+
+	// init Isotope
+	var $grid = $('.grid').isotope({
+	  itemSelector: '.element-item',
+	  layoutMode: 'fitRows'
+	});
+	// filter functions
+	var filterFns = {};
+
+	// bind filter button click
+	$('.filters-button-group').on( 'click', 'button', function() {
+	  var filterValue = $( this ).attr('data-filter');
+
+	  // use filterFn if matches value
+	  filterValue = filterFns[ filterValue ] || filterValue;
+	  $grid.isotope({ filter: filterValue });
+	});
+	
+	// change is-checked class on buttons
+	$('.button-group').each( function( i, buttonGroup ) {
+	  var $buttonGroup = $( buttonGroup );
+	  $buttonGroup.on( 'click', 'button', function() {
+	    $buttonGroup.find('.is-checked').removeClass('is-checked');
+	    $( this ).addClass('is-checked');
+	  });
+	});
+
+
 
 
 	
